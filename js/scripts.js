8e51692ad5b299b6271bb142bfa66509182de007
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-01-09 14:01:52 -0800

    add operators - addition, subtraction, multiplication, division to calculator functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
-function saySomething(whatToSay){
-    window.alert(whatToSay);
-}
-
+// Business Logic //
 function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("hi");
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-saySomething("The sum is " + add(3,5) + ".");
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) { 
+  return number1 / number2;
+}
+
+
+
+// User Interface Logic //
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+
+window.alert("Your numbers added together equal: " + add(number1, number2) +
+    ". Your numbers subtracted equal: " + subtract(number1, number2) +
+    ". Your numbers multiplied equal: " + multiply(number1, number2) +
+    ". Your numbers divided equal: " + divide(number1, number2))
 
 
---
+++ readme.md
@@ -0,0 +1,31 @@
+Calculator
+
+By: Jannon Sielaff
+and: Justin Lee
+
+It's a calculator, Homie
+
+## Technologies Used
+
+JavaScript
+HTML
+CSS
+Web Browser
+
+## Description
+
+It's just a basic calculator.  
+
+## Setup/Installation Requirements
+
+It will open in a browser window with prompts, so you'll need to download the repository, and then load index.html into your web browser.  Then the files and scripts should do their thing!
+
+## Known Bugs
+
+None, because I am the best.
+
+## License
+
+Feel free to message me with any comments or concerns or questions!  jannon.sielaff@gmail.com! 
+
+Copyright (c) _January 9th, 2023_ _Jannon Sielaff_
